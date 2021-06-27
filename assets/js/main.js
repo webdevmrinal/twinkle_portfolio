7be6149a4544085d78d3357206623c2f3ab7f3d8
printf-twinkle <twinklechokostar@gmail.com>
2021-06-27 15:55:42 +0530

    adding the toggle button but does not function properly

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
-
+function toggleDarkMode(toggleSwitch) {
+  var htmlTag = document.getElementById("html");
+  console.log("html classes", htmlTag.classList);
+  console.log(toggleSwitch.checked);
+  if (toggleSwitch.checked) {
+    htmlTag.classList.add("dark-mode");
+  } else {
+    htmlTag.classList.remove("dark-mode");
+  }
+}
 !(function($) {
   "use strict";
 
---
+++ assets/js/switch_mode.js
@@ -0,0 +1,10 @@
+// function toggleDarkMode(toggleSwitch) {
+//     var htmlTag = document.getElementById("html");
+//     console.log("html classes", htmlTag.classList);
+//     console.log(toggleSwitch.checked);
+//     if (toggleSwitch.checked) {
+//       htmlTag.classList.add("dark-mode");
+//     } else {
+//       htmlTag.classList.remove("dark-mode");
+//     }
+//   }
