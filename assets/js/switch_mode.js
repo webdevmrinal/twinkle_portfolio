0c9d2a2275d2e5cd8fa4b2967822c9a5e8ad5ece
webdevmrinal <mrinal.webdev@gmail.com>
2024-03-29 18:40:38 +0530

    fixed dark mode and css colors accordingly

--- assets/js/switch_mode.js
+++ assets/js/switch_mode.js
@@ -1,10 +1,10 @@
 // function toggleDarkMode(toggleSwitch) {
 //     var htmlTag = document.getElementById("html");
-//     console.log("html classes", htmlTag.classList);
+//     // console.log("html classes", htmlTag.classList);
 //     console.log(toggleSwitch.checked);
-//     if (toggleSwitch.checked) {
+//     if (toggleSwitch.checked && htmlTag !== null) {
 //       htmlTag.classList.add("dark-mode");
-//     } else {
+//     } else if(htmlTag !== null) {
 //       htmlTag.classList.remove("dark-mode");
 //     }
 //   }
